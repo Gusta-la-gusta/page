0ae98ac455fb21537ae8e9cd5a86208186748859
Gusta-la-gusta <gustavogomesdas1210@gmail.com>
2025-06-28 07:09:14 -0300

    Delete contagem enem.js

--- contagem enem.js
+++
@@ -1,7 +0,0 @@
-function atualizarContadorEnem() {
-  const dataEnem = new Date("2025-11-02"); // Ex: 2 de nov de 2025
-  const hoje = new Date();
-  const diff = Math.ceil((dataEnem - hoje) / (1000 * 60 * 60 * 24));
-  document.getElementById("contador-enem").textContent = diff;
-}
-document.addEventListener("DOMContentLoaded", atualizarContadorEnem);
